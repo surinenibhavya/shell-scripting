be318df3919c8eddb6b3d70b469b8d7bad0f80eb
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-29 19:53:52 +0530

    updating ./shell-scripting

--- roboshop=project/components/frontend.sh
+++ roboshop=project/components/frontend.sh
@@ -6,7 +6,6 @@
 # ​
 # Let's download the HTDOCS content and deploy under the Nginx path.
 # # curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-#
 # ​
 # Deploy the downloaded content in Nginx Default Location.
 # # cd /usr/share/nginx/html
@@ -15,9 +14,9 @@
 # # mv frontend-main/static/* .
 # # mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 echo "Installing NGINX"
-yum install nginx -y >/tmp/roboshop.log
+yum install nginx -y >>/tmp/roboshop.log
 
 echo "Frontend content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
 
-
+echo ""
