0faee0090924ab5a22d0fd2f141f76e2b5f2fe53
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-29 20:20:27 +0530

    updating ./shell-scripting

--- roboshop=project/components/frontend.sh
+++ roboshop=project/components/frontend.sh
@@ -20,3 +20,9 @@ yum install nginx -y &>>$LOG_FILE
 echo "Frontend content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 
+echo "Clean old data"
+rm -rf /usr/share/nginx/html* &>>$LOG_FILE
+
+echo "Extend Frontend content"
+cd /usr/share/nginx/html &>>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
