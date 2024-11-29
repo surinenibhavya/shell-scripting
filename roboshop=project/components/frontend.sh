842a7c86851a90cd6d581a9c3382fabb4b1c679f
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-29 20:43:20 +0530

    updating ./shell-scripting

--- roboshop=project/components/frontend.sh
+++ roboshop=project/components/frontend.sh
@@ -13,3 +13,13 @@ rm -rf /usr/share/nginx/html* &>>$LOG_FILE
 echo "Extend Frontend content"
 cd /tmp
 unzip -o frontend.zip &>>$LOG_FILE
+
+echo "Copy extracted content to nginx path"
+cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+
+echo "Copy nginx roboshop config"
+cp -r frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+
+echo "Start nginx service"
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
