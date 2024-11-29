e8830d2d2a667c2ee2567cbaddf82c73a46e9513
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-29 18:48:30 +0530

    updating ./shell-scripting

--- roboshop=project/components/frontend.sh
+++ roboshop=project/components/frontend.sh
@@ -14,7 +14,9 @@
 # # unzip /tmp/frontend.zip
 # # mv frontend-main/static/* .
 # # mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+echo "Installing NGINX"
 yum install nginx -y
+
 
 
 
