ddf166e66f0acf4b8375cb7e43952e91eeadc7c2
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-29 18:54:44 +0530

    updating ./shell-scripting

--- roboshop=project/components/frontend.sh
+++ roboshop=project/components/frontend.sh
@@ -17,6 +17,10 @@
 echo "Installing NGINX"
 yum install nginx -y >/tmp/roboshop.log
 
+echo "Front end content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
+
+
 
 
 
