c127b2ed5cc0b3c86aeb431e6964abcef84f2d34
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-29 12:32:44 +0530

    file exist or not

--- roboshop=project/roboshop.sh
+++ roboshop=project/roboshop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-bash components/$1.sh
+if [-f bash components/$1.sh]; then
+  bash components/$1.sh
+else
+  echo -e "\e[1;31mInvalid inputs\e[0m"
+  echo -e "\e[1;33mAvailable inputs-fronend|mongodb|catalogue|redis|user|cart|mysql|shipping|rabbitmq|payment|dispatch\e[0m"
+fi
