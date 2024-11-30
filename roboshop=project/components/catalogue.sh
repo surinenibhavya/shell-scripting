cbe466556c7d67995e15c00663c7cc880a7901b6
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-30 12:06:47 +0530

    updating ./shell-scripting

--- roboshop=project/components/catalogue.sh
+++ roboshop=project/components/catalogue.sh
@@ -1,1 +1,24 @@
-echo installing catalogue
+source components/common.sh
+
+echo "Setup Node JS file"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+
+echo "Install Node JS"
+yum install nodejs -y &>>$LOG_FILE
+
+echo "Create app user"
+useradd roboshop &>>$LOG_FILE
+
+echo "Download catalogue code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
+
+echo "Extract catalogue code"
+cd /tmp
+unzip -o catalogue.zip &>>$LOG_FILE
+
+echo "Copy catalogue code"
+cp -r catalogue-main /home/roboshop/catalogue &>>$LOG_FILE
+
+echo "Install Node JS dependencies"
+cd /home/roboshop/catalogue
+npm install &>>$LOG_FILE
