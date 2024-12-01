b5485546d293324e18dbdbc174f08254ef08f115
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-01 12:56:54 +0530

    updating ./shell-scripting

--- roboshop=project/components/catalogue.sh
+++ roboshop=project/components/catalogue.sh
@@ -31,6 +31,13 @@ chown roboshop:roboshop /home/roboshop/ -R
 echo "Update System file"
 sudo sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop-internal/' /home/roboshop/catalogue/systemd.service
 
-cat  /home/roboshop/catalogue/systemd.service
+echo "Setup catalogue file"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+
+echo "Start catalogue"
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
+
 
 
