9219b6e4a8fcae737018bc5ea9c6f23c0b29760b
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-01 12:51:00 +0530

    updating ./shell-scripting

--- roboshop=project/components/catalogue.sh
+++ roboshop=project/components/catalogue.sh
@@ -29,13 +29,7 @@ npm install &>>$LOG_FILE
 chown roboshop:roboshop /home/roboshop/ -R
 
 echo "Update System file"
-sed -i -e 's/MONGO_DSNAME/mongob.roboshop-internal/' /home/roboshop/catalogue/systemd.service &>>$LOG_FILE
+sed -i -e 's/MONGO_DSNAME/mongodb.roboshop-internal/' /home/roboshop/catalogue/systemd.service &>>$LOG_FILE
 
-echo "Setup catalogue file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 
-echo "Start catalogue"
-systemctl daemon-reload
-systemctl start catalogue
-systemctl enable catalogue
 
