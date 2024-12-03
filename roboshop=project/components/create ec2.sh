8c8c5af838cc684dabb75c7431eecd8b6c71a292
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-03 22:01:18 +0530

    updating ./shell-scripting

--- roboshop=project/components/create ec2.sh
+++ roboshop=project/components/create ec2.sh
@@ -1,1 +1,8 @@
-aws ec2 describe-images --owners amazon --filters "Name=name,Values=Centos-8-DevOps-Practice"
+#!/bin/bash
+
+AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]'
+)
+if [-z "$(AMI_ID)"]; then
+ echo "unable to finf image AMIID"
+ exit
+ 
