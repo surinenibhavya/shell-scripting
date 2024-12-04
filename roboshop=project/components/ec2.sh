8958bdd86fe16396520cc59bff1e86f7bac248d8
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 08:16:45 +0530

    updating ./shell-scripting

--- roboshop=project/components/ec2.sh
+++ roboshop=project/components/ec2.sh
@@ -2,16 +2,16 @@
 
 INSTANCE_NAME=$1
 if [ -z "${INSTANCE_NAME}" ]; then
- echo "Instance name argumnet is neeeded"
- exit
+  echo "Instance name argument is needed"
+  exit
 fi
 
-AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
- echo "unable to find image AMIID"
- exit
+  echo "Unable to find image AMI ID"
+  exit
 else
- echo AMI ID =${AMI_ID}
+  echo "AMI ID = ${AMI_ID}"
 fi
 
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
