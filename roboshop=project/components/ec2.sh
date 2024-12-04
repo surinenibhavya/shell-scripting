d33285a7233cfdcba27e7d5c8f21ea622905bb55
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 08:17:58 +0530

    updating ./shell-scripting

--- roboshop=project/components/ec2.sh
+++ roboshop=project/components/ec2.sh
@@ -3,7 +3,6 @@
 INSTANCE_NAME=$1
 if [ -z "${INSTANCE_NAME}" ]; then
   echo "Instance name argument is needed"
-  exit
 fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
