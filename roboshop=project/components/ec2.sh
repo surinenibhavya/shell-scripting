23a86bf171fb95e5e43f6ed1665d0fea9b7acb48
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 07:57:32 +0530

    updating ./shell-scripting

--- roboshop=project/components/ec2.sh
+++ roboshop=project/components/ec2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]'
-)
+AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
  echo "unable to find image AMIID"
 else
  echo AMI ID =${AMI_ID}
 fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+aws ec2 run-instances --image-id  ${AMI_ID} --instance-type t2.micro --output text
