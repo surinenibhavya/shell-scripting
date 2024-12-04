a10b6381365d150862f16749583fd22b1202bfc7
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 08:02:11 +0530

    updating ./shell-scripting

--- roboshop=project/components/ec2.sh
+++ roboshop=project/components/ec2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+INSTANCE_NAME=$1
+if [ -z "${INSTANCE_NAME}" ]; then
+ echo "Instanc ename argumnet is neeeded"
+fi
 
 AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
