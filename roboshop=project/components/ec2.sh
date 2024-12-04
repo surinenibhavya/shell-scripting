5142be7c85f119b77f130b9aa387fb319d995da5
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 11:24:15 +0530

    updating ./shell-scripting

--- roboshop=project/components/ec2.sh
+++ roboshop=project/components/ec2.sh
@@ -6,3 +6,12 @@ if [ -z "${AMI_ID}" ]; then
 else
   echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
 fi
+
+aws ec2 run-instances \
+    --image-id ami-0abcdef1234567890 \
+    --instance-type t2.micro \
+    --count 1 \
+    --subnet-id subnet-08fc749671b2d077c \
+    --key-name MyKeyPair \
+    --security-group-ids sg-0b0384b66d7d692f9 \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=server,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
