30a2fbd072568332b53275f38a1058566caba71a
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 11:30:31 +0530

    updating ./shell-scripting

--- roboshop=project/components/ec2.sh
+++ roboshop=project/components/ec2.sh
@@ -7,4 +7,4 @@ else
   echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ${AMI_ID} -instance-type t3.micro --output text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
