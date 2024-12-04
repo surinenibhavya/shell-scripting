602a4d487702e8aeb31a3131ab9442da4d0ed3f5
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 13:34:12 +0530

    updating ./shell-scripting

--- roboshop=project/ec2.sh
+++ roboshop=project/ec2.sh
@@ -16,7 +16,7 @@ fi
 
 PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name ,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
-if[ -z "${PRIVATE_IP}" ]; then
+if [ -z "${PRIVATE_IP}" ]; then
  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
 else
  echo "Already ${INSTANCE_NAME} is there"
