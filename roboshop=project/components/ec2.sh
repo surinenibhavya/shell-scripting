0cbae89726f393c38b0279e9a6ed5250ef131941
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-04 11:44:58 +0530

    updating ./shell-scripting

--- roboshop=project/components/ec2.sh
+++ roboshop=project/components/ec2.sh
@@ -9,7 +9,7 @@ fi
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
   echo -e "\e[1;31mUnable to find image AMI ID\e[0m"
-  exit 1
+  exit 
 else
   echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
 fi
