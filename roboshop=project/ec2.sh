50cc1d1137af34716f90f3e27f95059a75a540f4
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-07 06:51:33 +0530

    updating ./shell-scripting

--- roboshop=project/ec2.sh
+++ roboshop=project/ec2.sh
@@ -44,7 +44,7 @@ echo '{
                                    "ResourceRecords": [{ "Value": "IPADDRESS"}]
 }}]
 }' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/"  >/tmp/record.json
-ZONE_ID=(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal | awk {'print$1'} | awk -F / {'print $3'})
+ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal | awk '{print $1}' | awk -F / '{print $3}'
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE-ID--change-batch file://tmp/record.json --output text &>>$LOG
 echo "DNS Record created"
 
