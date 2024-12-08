c6e1487860ba6f3741f503c882e0017e4ac8b17b
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-08 07:48:55 +0530

    updating ./shell-scripting

--- roboshop=project/components/frontend.sh
+++ roboshop=project/components/frontend.sh
@@ -2,32 +2,24 @@ source components/common.sh
 
 echo "Installing NGINX"
 yum install nginx -y &>>$LOG_FILE
-STAT $?
 
 echo "Frontend content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
-STAT $?
 
 echo "Clean old data"
 rm -rf /usr/share/nginx/html* &>>$LOG_FILE
-STAT $?
 
 echo "Extend Frontend content"
 cd /tmp
 unzip -o frontend.zip &>>$LOG_FILE
-STAT $?
-
 
 echo "Copy Extracted Content to Nginx Path"
 cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
-STAT $?
-
 
 echo "Copy nginx roboshop config"
 cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-STAT $?
 
 echo "Start nginx"
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
-STAT $?
+
