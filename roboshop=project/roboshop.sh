5632b153d2a45952ef832995f6028582ef6164bd
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-08 15:58:35 +0530

    updating ./shell-scripting

--- roboshop=project/roboshop.sh
+++ roboshop=project/roboshop.sh
@@ -2,7 +2,7 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]; then
  echo "you should be root user to execute the script"
- exit
+ exit 1
 fi
 
 if [ -f components/$1.sh ]; then
@@ -10,5 +10,6 @@ if [ -f components/$1.sh ]; then
 else
   echo -e "\e[1;31mInvalid inputs\e[0m"
   echo -e "\e[1;33mAvailable inputs-frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|rabbitmq|payment|dispatch\e[0m"
+  exit 1
 fi
 
