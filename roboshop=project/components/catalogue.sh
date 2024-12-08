109e70d8b3cbc238dc71bfa8c7e600bb89b2b4b0
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-08 20:28:28 +0530

    updating ./shell-scripting

--- roboshop=project/components/catalogue.sh
+++ roboshop=project/components/catalogue.sh
@@ -1,41 +1,51 @@
 source components/common.sh
 
 echo "Setup NodeJS repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+STAT $?
 
 echo "Install NodeJS"
-yum install nodejs -y
+yum install nodejs -y &>>$LOG_FILE
+STAT $?
 
 echo "Create App user"
 useradd roboshop
+STAT $?
 
 echo "Download catalogue code"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
+STAT $?
 
 echo "Extract catalogue code"
 cd /tmp/
-unzip -o catalogue.zip
+unzip -o catalogue.zip &>>$LOG_FILE
+STAT $?
 
 echo "Clean old catalogue"
-rm -rf  /home/roboshop/catalogue
+rm -rf  /home/roboshop/catalogue &>>$LOG_FILE
+STAT $?
 
 echo "Copy catalogue content"
-cp -r catalogue-main /home/roboshop/catalogue
+cp -r catalogue-main /home/roboshop/catalogue &>>$LOG_FILE &>>$LOG_FILE
+STAT $?
 
 echo "Install NodeJS Dependencies"
 cd /home/roboshop/catalogue
 npm install &>>$LOG_FILE
+STAT $?
 
-chown roboshop:roboshop /home/roboshop/ -R
+chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
 
 echo "Update systemd file"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
-
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG_FILE
+STAT $?
 
 echo "Setup catalogue systemd file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+STAT $?
 
 echo "Start catalogue"
-systemctl daemon-reload
-systemctl start catalogue
-systemctl enable catalogue
+systemctl daemon-reload &>>$LOG_FILE
+systemctl start catalogue &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
+STAT $?
