1684673250fe6c7ec6f83903ce046dab898f37a5
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-09 16:59:00 +0530

    updating ./shell-scripting

--- roboshop=project/ec2.sh
+++ roboshop=project/ec2.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-
 LOG=/tmp/instance-create.log
 rm -f $LOG
+
 if [ "$1" == "list" ]; then
   aws ec2 describe-instances --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table
   exit
@@ -9,8 +8,8 @@ fi
 
 INSTANCE_NAME=$1
 if [ -z "${INSTANCE_NAME}" ]; then
- echo "Input argument needed"
- exit
+  echo "Input argument needed"
+  exit
 fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
@@ -22,31 +21,32 @@ else
 fi
 
 PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name ,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-
 if [ -z "${PRIVATE_IP}" ]; then
- SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all-ports --query "SecurityGroups[*].GroupId" --output text)
- if [ -z "${SG_ID}" ]; then
-   echo "Security group does not exist"
-   exit
- fi
- aws ec2 run-instances --image-id "${AMI_ID}" --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}" &>>$LOG
- echo "Instance created"
+  SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all-ports --query "SecurityGroups[*].GroupId" --output text)
+  if [ -z "${SG_ID}" ]; then
+    echo "Security group does not exist"
+    exit
+  fi
+  aws ec2 run-instances --image-id "${AMI_ID}" --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}" &>>$LOG
+  echo "Instance created"
 else
- echo "Already ${INSTANCE_NAME} is there"
+  echo "Already ${INSTANCE_NAME} is there"
 fi
 
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
- echo '{
-              "Comment": "CREATE/DELETE/UPSERT a record ",
-              "Changes": [{
-              "Action": "UPSERT",
-                          "ResourceRecordSet": {
-                                      "Name": "DNSNAME.roboshop.internal",
-                                      "Type": "A",
-                                      "TTL": 300,
-                                   "ResourceRecords": [{ "Value": "IPADDRESS"}]
-  }}]
-  }' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/"  >/tmp/record.json
+echo '{
+  "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "DNSNAME.roboshop.internal",
+      "Type": "A",
+      "TTL": 300,
+      "ResourceRecords": [{ "Value": "IPADDRESS" }]
+    }
+  }]
+}' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/"  >/tmp/record.json
+
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal | awk '{print $1}' | awk -F / '{print $3}')
-aws route53 change-resource-record-sets --hosted-zone-id $ZONE-ID--change-batch file://tmp/record.json --output text &>>$LOG
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text &>>$LOG
 echo "DNS Record created"
