c14bdf1fb9a97d88bef4d2a79482bab6f1c6b615
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-10 07:03:02 +0530

    updating ./shell-scripting

--- roboshop=project/components/mysql.sh
+++ roboshop=project/components/mysql.sh
@@ -1,1 +1,15 @@
-echo installing mysql
+source components/common.sh
+
+echo "Setting up Repo file"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
+dnf module disable mysql &>>$LOG_FILE
+STAT $?
+
+echo "Downloading mysql"
+yum install mysql-community-server -y &>>$LOG_FILE
+STAT $?
+
+echo "Start mysql"
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld &>>$LOG_FILE
+STAT $?
