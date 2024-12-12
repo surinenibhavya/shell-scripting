d3fb1993da02f169da97573d503f9301139a59ac
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-12 16:32:42 +0530

    updating ./shell-scripting

--- roboshop=project/components/catalogue.sh
+++ roboshop=project/components/catalogue.sh
@@ -1,3 +1,9 @@
 source components/common.sh
 
-NODEJS catalogue
+echo "Setup NodeJS repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+STAT $?
+
+echo "Install NodeJS"
+yum install nodejs -y &>>$LOG_FILE
+STAT $?
