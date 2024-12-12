c7901e6271aec3323443172d119dbb2ce15fe25c
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-12 17:56:55 +0530

    updating ./shell-scripting

--- roboshop=project/components/rabbitmq.sh
+++ roboshop=project/components/rabbitmq.sh
@@ -1,14 +1,12 @@
 source components/common.sh
 
-echo "Setup YUM repositories for RabbitMQ"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>>$LOG_FILE
+echo "Configure YUM repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
 STAT $?
 
-echo "Install erland and rabbit mq"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm &>>$LOG_FILE
-yum install erlang -y &>>$LOG_FILE
+echo "Install RabbitMQ & ErLang"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>$LOG_FILE
 STAT $?
-
 
 echo "Start RabbitMQ"
 systemctl enable rabbitmq-server &>>$LOG_FILE
