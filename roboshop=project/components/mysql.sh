f81ed7e0fd164c801099081eda2178c85bcf7f90
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-12-12 17:19:32 +0530

    updating ./shell-scripting

--- roboshop=project/components/mysql.sh
+++ roboshop=project/components/mysql.sh
@@ -1,32 +1,30 @@
 source components/common.sh
 
-echo "Setting up Repo file"
+echo "Setting Up MySQL Repo"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
-dnf module disable mysql &>>$LOG_FILE
 STAT $?
 
-echo "Downloading mysql"
+echo "Install MySQL Server"
 yum install mysql-community-server -y &>>$LOG_FILE
 STAT $?
 
-echo "Start mysql"
+echo "Start MySQL Service"
 systemctl enable mysqld &>>$LOG_FILE
 systemctl start mysqld &>>$LOG_FILE
 STAT $?
 
-DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log |awk '{print $NF}')
+DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');
 uninstall plugin validate_password;" >/tmp/pass.sql
-STAT $?
 
-echo "Change Default password"
-echo "show databases;" | mysql -uroot -pRoboShop@1 &>>$LOG_FILE
+echo "Change Default Password"
+echo 'show databases;' | mysql -uroot -pRoboShop@1 &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-  mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" >/tmp/pass.sql &>>$LOG_FILE
+  mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/pass.sql &>>$LOG_FILE
 fi
 STAT $?
 
-echo "Download the schema"
+echo "Download MySQL Shipping Schema"
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
 STAT $?
 
